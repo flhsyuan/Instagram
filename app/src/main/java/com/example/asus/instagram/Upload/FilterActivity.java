3b483f27b2ea89578e11ab3f2b5f9dbe10a795cb
RobinWYQ <475165031@qq.com>
2018-10-18 17:07:11 +1100

    2018-10-18-17:06

--- app/src/main/java/com/example/asus/instagram/Upload/FilterActivity.java
+++ app/src/main/java/com/example/asus/instagram/Upload/FilterActivity.java
@@ -99,28 +99,6 @@ public class FilterActivity extends AppCompatActivity implements FiltersListFrag
         });
     }
 
-    //通过路径将图片转化为Bitmap
-//    public static Bitmap getBitmapFromUri(String path, int w, int h) {
-//        BitmapFactory.Options opts = new BitmapFactory.Options();
-//        opts.inJustDecodeBounds = true;
-//        opts.inPreferredConfig = Bitmap.Config.ARGB_8888;
-//        BitmapFactory.decodeFile(path, opts);
-//        int width = opts.outWidth;
-//        int height = opts.outHeight;
-//        float scaleWidth = 0.f, scaleHeight = 0.f;
-//        if (width > w || height > h) {
-//            scaleWidth = ((float) width) / w;
-//            scaleHeight = ((float) height) / h;
-//        }
-//        opts.inJustDecodeBounds = false;
-//        float scale = Math.max(scaleWidth, scaleHeight);
-//        opts.inSampleSize = (int) scale;
-//        WeakReference<Bitmap> weak = new WeakReference<Bitmap>(
-//                BitmapFactory.decodeFile(path, opts));
-//        return Bitmap.createScaledBitmap(weak.get(), w, h, true);
-//    }
-
-
     private void loadImage() throws IOException {
         intent = getIntent();
         if (intent.hasExtra(getString(R.string.selected_image))) {
