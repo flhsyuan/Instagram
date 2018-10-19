ecdda531c69369dd500008131429c59f7d5022a4
Jercylv <lyuy4@student.unimelb.edu.au>
2018-10-19 22:11:31 +1100

    for demo

--- app/src/main/java/com/example/asus/instagram/Upload/ShareActivity.java
+++ app/src/main/java/com/example/asus/instagram/Upload/ShareActivity.java
@@ -35,7 +35,7 @@ public class ShareActivity extends AppCompatActivity{
     //firebase
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    private FirebaseDa    tabase mFirebaseDB;
+    private FirebaseDatabase mFirebaseDB;
     private DatabaseReference myRef;
     private FirebaseMethods mFirebaseMethods;
 
