ef0ffba7bb4d47625d56958768d5b7936eadda85
Jercylv <lyuy4@student.unimelb.edu.au>
2018-10-21 10:20:26 +1100

    activity feed related layout added

---
+++ app/src/main/java/com/example/asus/instagram/Feed/YouFragment.java
@@ -0,0 +1,11 @@
+package com.example.asus.instagram.Feed;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author : Yujie Lyu
+ * @date : 21-10-2018
+ * @time : 09:39
+ */
+public class YouFragment extends Fragment {
+}
