cb195fbd8e0761d619aa60a60630925b0c54b7ce
Jercylv <lyuy4@student.unimelb.edu.au>
2018-10-21 10:21:05 +1100

    activity feed related layout added

--- app/src/main/java/com/example/asus/instagram/Feed/FeedActivity.java
+++ app/src/main/java/com/example/asus/instagram/Feed/FeedActivity.java
@@ -4,33 +4,63 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
+import com.example.asus.instagram.Home.HomeFragment;
+import com.example.asus.instagram.Home.PositionFragment;
 import com.example.asus.instagram.R;
 import com.example.asus.instagram.Utils.BottomNavigationViewHelper;
+import com.example.asus.instagram.Utils.SectionsPagerAdapter;
 
 public class FeedActivity extends AppCompatActivity {
     private static final String TAG = "FeedActivity";
     private static final int ACTIVITY_NUM = 3;
 
     private Context mContext = FeedActivity.this;
+    //widgets
+    private ViewPager mViewPager;
+    private FrameLayout mFramLayout;
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_activity_feed);
         Log.d(TAG, "onCreate: started");
 
         setupBottomNavigationView();
+        setupViewPager();
     }
 
     @Override
     protected void onPause() {
         overridePendingTransition(0,0);
         super.onPause();
+    }
+
+    private void setupViewPager() {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FollowingFragment());// Following fragment :index 0
+        adapter.addFragment(new YouFragment()); //You fragment :index 1
+        mViewPager = (ViewPager) findViewById(R.id.viewpager_container);
+        mViewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+//        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+//        tabLayout.getTabAt(1).setIcon(R.drawable.ic_home);
+
+        tabLayout.getTabAt(0).setText("Following");
+        tabLayout.getTabAt(1).setText("You");
+
     }
 
     /**
