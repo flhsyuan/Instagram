4f542e0cb9748957cd7504eb774e71db26c67811
RobinWYQ <475165031@qq.com>
2018-10-21 14:40:27 +1100

    2018-10-21-14:39

--- app/src/main/java/com/example/asus/instagram/Feed/YouFragment.java
+++ app/src/main/java/com/example/asus/instagram/Feed/YouFragment.java
@@ -8,9 +8,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.example.asus.instagram.Models.Comment;
+import com.example.asus.instagram.Models.Like;
+import com.example.asus.instagram.Models.Photo;
+import com.example.asus.instagram.Models.YouFeed;
 import com.example.asus.instagram.R;
+import com.example.asus.instagram.Utils.GridImageAdapter;
+import com.example.asus.instagram.Utils.MainfeedListAdapter;
+import com.example.asus.instagram.Utils.YouFeedAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,6 +28,11 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static android.support.constraint.Constraints.TAG;
 
@@ -29,9 +42,14 @@ import static android.support.constraint.Constraints.TAG;
  * @time : 09:39
  */
 public class YouFragment extends Fragment {
-    
+
+    private ArrayList<YouFeed> mYouFeed;
+    private ArrayList<YouFeed> mPaginatedFeeds;
     private ListView mListView;
     private ArrayList<String> mFollower;
+    private int mResults;
+    private YouFeedAdapter mAdapter;
+    private ArrayList<Photo> mPhotos;
 
     @Nullable
     @Override
@@ -39,7 +57,10 @@ public class YouFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
         mListView = (ListView) view.findViewById(R.id.listView);
         mFollower = new ArrayList<>();
+        mYouFeed = new ArrayList<>();
+        mPhotos = new ArrayList<>();
         getFollower();
+        getPhotos();
 
 
         return view;
@@ -64,7 +85,6 @@ public class YouFragment extends Fragment {
                     mFollower.add(singleSnapshot.child(getString(R.string.field_user_id)).getValue().toString());
 
                 }
-                //getPhotos();
 
             }
 
@@ -74,4 +94,110 @@ public class YouFragment extends Fragment {
             }
         });
     }
+
+    private void getPhotos(){
+        Log.d(TAG, "getFollower: search for follower.");
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        //retrieval photos according to the user ID using the user_photos in firebase DB
+        Query query = reference.child(getString(R.string.dbname_user_photos)).child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override      //each snapshot here represents a user object.
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot ds: dataSnapshot.getChildren() ){
+
+                    Photo photo = new Photo();
+                    Map<String,Object> objectMap = (HashMap<String,Object>) ds.getValue();
+
+                    try{
+                        photo.setCaption(objectMap.get(getString(R.string.field_caption)).toString());
+                        photo.setTags(objectMap.get(getString(R.string.field_tags)).toString());
+                        photo.setPhoto_id(objectMap.get(getString(R.string.field_photo_id)).toString());
+                        photo.setDate_created(objectMap.get(getString(R.string.field_date_created)).toString());
+                        photo.setUser_id(objectMap.get(getString(R.string.field_user_id)).toString());
+                        photo.setImage_path(objectMap.get(getString(R.string.field_image_path)).toString());
+
+                        ArrayList<Comment> comments = new ArrayList<Comment>();
+                        for (DataSnapshot dSnapshot: ds
+                                .child(getString(R.string.field_comments)).getChildren()){
+                            Comment comment = new Comment();
+
+                            comment.setUser_id(dSnapshot.getValue(Comment.class).getUser_id());
+                            comment.setComment(dSnapshot.getValue(Comment.class).getComment());
+                            comment.setDate_created(dSnapshot.getValue(Comment.class).getDate_created());
+                            comments.add(comment);
+                        }
+
+                        photo.setComments(comments);
+
+                        List<Like> likeList = new ArrayList<Like>();
+                        for (DataSnapshot dSnapshot: ds
+                                .child(getString(R.string.field_likes)).getChildren()){
+                            Like like = new Like();
+                            like.setUser_id(dSnapshot.getValue(Like.class).getUser_id());
+                            likeList.add(like);
+
+                            //add to youfeed
+                            YouFeed youFeed = new YouFeed();
+                            youFeed.setUser_id(dSnapshot.getValue(Like.class).getUser_id());
+                            youFeed.setDate_created(dSnapshot.getValue(Like.class).getDate_created());
+                            youFeed.setPhoto_id(photo.getPhoto_id());
+                            mYouFeed.add(youFeed);
+                        }
+                        photo.setLikes(likeList);
+                        mPhotos.add(photo);
+                    }
+                    catch (NullPointerException e){
+                        Log.d(TAG, "onDataChange: NullPointerException ");
+                    }
+                }
+                System.out.println(mYouFeed);
+                displayYouFeeds();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG, "onCancelled: query is stop ");
+            }
+        });
+    }
+
+
+    private void displayYouFeeds(){
+        mPaginatedFeeds = new ArrayList<>();
+
+        if(mYouFeed!=null){
+            try{
+                Collections.sort(mYouFeed, new Comparator<YouFeed>() {
+                    @Override
+                    public int compare(YouFeed o1, YouFeed o2) {
+                        return o2.getDate_created().compareTo(o1.getDate_created());
+                    }
+                });
+
+                int iterations = mYouFeed.size();
+
+                if(iterations > 20){
+                    iterations = 20;
+                }
+
+                mResults = 20;
+                for(int i = 0; i < iterations; i++){
+                    mPaginatedFeeds.add(mYouFeed.get(i));
+                }
+
+                mAdapter = new YouFeedAdapter(getActivity(), R.layout.layout_you_list_activity_feed, mPaginatedFeeds);
+                mListView.setAdapter(mAdapter);
+
+            }catch (NullPointerException e){
+                Log.e(TAG, "displayPhotos: NullPointerException" + e.getMessage());
+            }catch (IndexOutOfBoundsException e){
+                Log.e(TAG, "displayPhotos: IndexOutOfBoundsException" + e.getMessage());
+            }
+        }
+    }
+
+
 }
+
--- app/src/main/java/com/example/asus/instagram/Utils/YouFeedAdapter.java
+++ app/src/main/java/com/example/asus/instagram/Utils/YouFeedAdapter.java
@@ -1,8 +1,11 @@
 package com.example.asus.instagram.Utils;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,86 +13,185 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.asus.instagram.Models.Photo;
+import com.example.asus.instagram.Models.User;
+import com.example.asus.instagram.Models.UserAccountSettings;
+import com.example.asus.instagram.Models.YouFeed;
+import com.example.asus.instagram.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class YouFeedAdapter extends ArrayAdapter<YouFeed>{
-    private ArrayList<YouActivityFeed> data = new ArrayList<YouActivityFeed>();
-    private Context context;
-    private int layoutResourceId;
+    private static final String TAG= "UserListAdapter";
 
-    public YouFeedAdapter(Context context, int resource, ArrayList<YouActivityFeed> data) {
-        super(context, resource, data);
-        this.layoutResourceId = resource;
-        this.context = context;
-        this.data = data;
+    private LayoutInflater mInflater;
+    private List<YouFeed> mYouFeed = null;
+    private int layoutResource;
+    private Context mContext;
+
+
+    public YouFeedAdapter(@NonNull Context context, int resource, List<YouFeed> objects) {
+        super(context, resource, objects);
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mContext = context;
+        layoutResource = resource;
+        this.mYouFeed = objects;
+        System.out.println("adapter:");
+        System.out.println(mYouFeed);
+
     }
 
 
-    @Override
-    public int getCount() {
-        return data.size();
+    private static class ViewHolder{
+        TextView username;
+        ImageView profileImage, myPhoto;
     }
 
+
+    @NonNull
     @Override
-    public YouActivityFeed getItem (int position){
-        return data.get(position);
-    }
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        final YouFeedAdapter.ViewHolder holder;
 
-    @Override
-    public long getItemId(int position) {
+        if(convertView ==null){
+            convertView = mInflater.inflate(layoutResource, parent,false);
+            holder = new YouFeedAdapter.ViewHolder();
 
-        return position;
-    }
+            holder.username = (TextView) convertView.findViewById(R.id.tv_description);
+            holder.profileImage = (ImageView) convertView.findViewById(R.id.iv_follower_photo);
+            holder.myPhoto = (ImageView) convertView.findViewById(R.id.iv_photo);
 
-    @Override
-    public int getPosition(YouActivityFeed item) {
-        return super.getPosition(item);
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View row = convertView;
-        ViewHolder holder = null;
-
-        if (row == null) {
-            LayoutInflater inflater = ((FragmentActivity) context).getLayoutInflater();
-            row = inflater.inflate(layoutResourceId, parent, false);
-            holder = new ViewHolder();
-            holder.profilepic = (ImageView) row.findViewById(R.id.iv_follower_photo);
-            holder.uploadedphoto = (ImageView) row.findViewById(R.id.iv_unfollow);
-            holder.description = (TextView) row.findViewById(R.id.tv_description);
-
-            row.setTag(holder);
-        } else {
-            holder = (ViewHolder) row.getTag();
+            convertView.setTag(holder);
+        }else {
+            holder = (YouFeedAdapter.ViewHolder) convertView.getTag();
         }
 
-        YouActivityFeed item = (YouActivityFeed) data.get(position);
 
-        String timePublish="";
-        Bitmap profilepic_bitmap = Utils.getBitmap(item.getUrlProfilePic());
-        holder.profilepic.setImageBitmap(profilepic_bitmap);
-        if (item.getUrlPhoto()!=null) {
-            Bitmap uploadpic_bitmap = Utils.getBitmap(item.getUrlPhoto());
-            holder.uploadedphoto.setImageBitmap(uploadpic_bitmap);
-            timePublish = Utils.getElapsedtime(item.getTimePublication(), "short");
-        }
-        else
-        {
-            holder.uploadedphoto.setImageResource(R.drawable.btn_user_followed);
-        }
-        holder.description.setText(Html.fromHtml("<font color='#2B547E'><b>" + item.getUsername() + "</b></font>" + " " +"<font color='#2C3539'>"+ item.getType() + item.getComment() +"</font>"+ " "  + timePublish ));
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        Query query = reference.child(mContext.getString(R.string.dbname_user_account_settings))
+                .orderByChild(mContext.getString(R.string.field_user_id))
+                .equalTo(getItem(position).getUser_id());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot singleSnapshot: dataSnapshot.getChildren()){
+                    Log.d(TAG, "onDataChange: "+
+                            singleSnapshot.getValue(UserAccountSettings.class).toString());
 
-        return row;
+                    System.out.println("profile:" + singleSnapshot.getValue(UserAccountSettings.class).getProfile_photo());
+                    ImageLoader imageLoader = ImageLoader.getInstance();
+
+                    imageLoader.displayImage(singleSnapshot.getValue(UserAccountSettings.class).getProfile_photo(),
+                            holder.profileImage);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        Query query2 = reference.child(mContext.getString(R.string.dbname_photos))
+                .orderByChild(mContext.getString((R.string.field_photo_id)))
+                .equalTo(getItem(position).getPhoto_id());
+        query2.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot singleSnapshot: dataSnapshot.getChildren()){
+
+                    Map<String, Object> objectMap = (HashMap<String, Object>) singleSnapshot.getValue();
+
+                    ImageLoader imageLoader = ImageLoader.getInstance();
+
+                    imageLoader.displayImage(objectMap.get("image_path").toString(),
+                            holder.myPhoto);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //get the user object
+        Query userQuery = reference
+                .child(mContext.getString(R.string.dbname_users))
+                .orderByChild(mContext.getString(R.string.field_user_id))
+                .equalTo(getItem(position).getUser_id());
+        userQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+
+            // get the liked user name from firebase
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                    Log.d(TAG, "onDataChange: found user: " +
+                            singleSnapshot.getValue(User.class).getUsername());
+
+                    holder.username.setText(singleSnapshot.getValue(User.class).getUsername());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        return convertView;
     }
 
-    static class ViewHolder {
-        ImageView profilepic;
-        ImageView uploadedphoto;
-        TextView description;
+    /**
+     * calculate how many days the photo was updated
+     */
+    private String getUpdatedTime(Photo photo)  {
+        final String photoTime = photo.getDate_created();
 
+        Log.d(TAG, "getUpdatedTime: calculating how many days the photo was updated ");
+        String period = "";
+        Calendar c =Calendar.getInstance();
+        Date today = c.getTime();
+        SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        timeFormat.setTimeZone(TimeZone.getTimeZone("Australia/Melbourne"));
+        timeFormat.format(today);
+
+        Date timeStamp;
+        try {
+            timeStamp = timeFormat.parse(photoTime);
+            period = String.valueOf(Math.round( today.getTime() - timeStamp.getTime() )/1000/60/60/24);
+
+        }catch (ParseException p){
+            Log.d(TAG, "getUpdatedTime: Parsing time data form DB failed");
+            period = "0";
+        }
+
+        return  period;
     }
+
 }
 
 
